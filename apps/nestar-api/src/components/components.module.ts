aed5cd8741309ce2a596986413cf625b499e54f0
Max-Mukhiddin <@mukhiddinsolijonov101@gmail.com>
2026-01-16 03:23:58 +0900

    feat: develop removeCommentByAdmin GraphQL API

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
 import { AuthModule } from './auth/auth.module';
-import { BoardArticleModule } from './board-article/board-article.module';
+import { CommentModule } from './comment/comment.module';
 import { LikeModule } from './like/like.module';
 import { ViewModule } from './view/view.module';
-import { CommentModule } from './comment/comment.module';
 import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
 	imports: [
