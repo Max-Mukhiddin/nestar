0b904a02294f3334ef1dd4f9c0ed6e105858c787
Max-Mukhiddin <@mukhiddinsolijonov101@gmail.com>
2026-02-10 19:37:34 +0900

    fix: comment.service

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -28,7 +28,7 @@ export class CommentService {
 		try {
 			result = await this.commentModel.create(input);
 		} catch (err) {
-			console.log('Error, Service.model:', err.message);
+			console.log('Error, Service.model:', err);
 			throw new BadRequestException(Message.CREATE_FAILED);
 		}
 
